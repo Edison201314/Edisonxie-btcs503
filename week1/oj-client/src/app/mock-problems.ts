399975fdbd76a0a7f51c72cfb3a7354086755c2a
edison <xiexin201314@gmail.com>
2017-03-01 06:18:48 -0800

    week1

---
+++ week1/oj-client/src/app/mock-problems.ts
@@ -0,0 +1,21 @@
+import {Problem} from "./models/problem.model";
+
+export const PROBLEMS : Problem[]=[{
+  id:1,
+  name:"Two Sum",
+  desc:"Given an array of integers, find two numbers such that they add up to a specific target number",
+  difficulty:"easy"
+},
+  {
+    id:2,
+    name:"3Sum",
+    desc:"Given an array S of n integer, are there elements a,b,c in S such that a+b+c=?",
+    difficulty:"medium"
+  },
+  {
+    id:3,
+    name:"4Sum",
+    desc:"Given an array",
+    difficulty:"medium"
+
+  }];
---
+++ week1/oj-client/src/app/models/problem.model.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by edison on 2/28/17.
+ */
+
+export class Problem{
+  id: number;
+  name:string;
+  desc:string;
+  difficulty:string;
+
+
+}
